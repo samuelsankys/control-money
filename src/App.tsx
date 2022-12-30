f55cedbf9b52773a66f64089efa377e1a2398087
samuelsankys <ssankys3@gmail.com>
2022-12-29 22:44:09 -0300

    chore: config styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { GlobalStyle } from "./styles/globals";
 
 
 export function App() {
   return (
     <div className="App">
      <h1>Hello Word</h1>
+     <GlobalStyle />
     </div>
   );
 }
